9f62713265640f24aa4a982474d1674edcb30a29
OmarIsm6 <thevoz1996@gmail.com>
2016-04-03 16:55:39 +0200

    Tercera parte del programa

---
+++ Jarra.java
@@ -0,0 +1,15 @@
+package prJArra;
+
+public class Jarra {
+
+
+public void llenaDesde(Jarra j){
+		while((this.cantidad<this.capacidad)&&(j.cantidad>0)){
+			this.cantidad++;
+			j.cantidad--;
+		} 
+	
+	public String toString(){
+		return ("("+this.capacidad+","+this.cantidad+")");
+	}
+}
